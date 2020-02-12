9a17ad4470f2f4f9e181ba9063e76a52492478ec
Thomas Nielse <skt.thomas@gmail.com>
2020-02-12 14:27:16 +0100

    memoized fetching of users????

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceholder from '../apis/JSONPlaceholder'
+import _ from 'lodash';
 
 export const fetchPosts = () => async (dispatch) => {
     const response = await jsonPlaceholder.get('/posts');
@@ -6,13 +7,16 @@ export const fetchPosts = () => async (dispatch) => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data})
 }
 
-export const fetchUser = (id) => async(dispatch) => {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
-
-    dispatch({type: 'FETCH_USER', payload: response.data });
-}
-
+export const fetchUser = (id) => (dispatch) => {
+    _fetchUser(id, dispatch);
+};
     //const response = await jsonPlaceholder.get('/posts'); //This CANNOT BE USED! WE HAVE TO USE MIDDLEWARE FIRST!!
     //this is because the async and await statements make the return value turn out to be something way different at compile-time. See babeljs.io
     //it will not be an action object like below, but instead the request object. 
     //We cannot remove the async property, as it turns the response into a promise, leading to errors where the component will render before data has arrived.
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({type: 'FETCH_USER', payload: response.data });
+});
