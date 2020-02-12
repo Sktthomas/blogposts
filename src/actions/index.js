498581407f94541a330903e48c2ab0b31b16d2a1
Thomas Nielse <skt.thomas@gmail.com>
2020-02-12 13:31:30 +0100

    added postlist render of API response

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import jsonPlaceholder from '../apis/JSONPlaceholder'
 export const fetchPosts = () => async (dispatch) => {
     const response = await jsonPlaceholder.get('/posts');
 
-    dispatch({ type: 'FETCH_POSTS', payload: response})
+    dispatch({ type: 'FETCH_POSTS', payload: response.data})
 }
 
     //const response = await jsonPlaceholder.get('/posts'); //This CANNOT BE USED! WE HAVE TO USE MIDDLEWARE FIRST!!
--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,3 +1,10 @@
-export default () => { //you may not return undefined as a value of a state, therefore we create a reducer with dummy value first.
-    return -1;
+export default (state = [], action) => { //you may not return undefined as a value of a state, therefore we create a reducer with dummy value first. Or an empty default state value
+
+//normally you use switch cases to order the action types you can meet in the reducer. 
+switch (action.type) {
+    case 'FETCH_POSTS':
+        return action.payload;
+    default:
+        return state
+}
 };
